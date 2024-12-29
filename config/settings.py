ff19d1ad001d742c462e1754b466aff766b1a40e
WawinyEdwin <wawinyedwin44@gmail.com>
2024-12-29 14:23:40 +0300

    fix: staticfiles on render

--- config/settings.py
+++ config/settings.py
@@ -176,6 +176,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
+
+### Start app
+```
+python -m gunicorn config.asgi:application -k uvicorn.workers.UvicornWorker
+```
 &copy; LimitGroup 2024
