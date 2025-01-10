f1219587dd6d5a8b20d5042f132c70bdeab326ac
WawinyEdwin <wawinyedwin44@gmail.com>
2025-01-10 15:53:11 +0300

    fix: conflicting urls

--- wera/urls.py
+++ wera/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path("weras/<int:pk>/", wera_detail, name="wera_detail"),
     path("search/", search_wera, name="search_wera"),
     path("privacy-policy/", privacy_policy, name="privacy_policy"),
-    path("privacy-policy/", cookie_policy, name="cookie_policy"),
+    path("cookie-policy/", cookie_policy, name="cookie_policy"),
     path("advertising/", advertising, name="advertising"),
     path("report-a-problem/", report_a_problem, name="report_a_problem"),
 ]
