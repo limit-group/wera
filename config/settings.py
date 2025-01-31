13bec19a2e5f81de4fcdce04aaf59b2fa13d2c7b
WawinyEdwin <wawinyedwin44@gmail.com>
2025-01-31 16:39:43 +0300

    fix: make smtp creds config firendly

--- config/settings.py
+++ config/settings.py
@@ -201,8 +201,8 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_FORMS = {"signup": "wera.forms.CustomSignupForm"}
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
